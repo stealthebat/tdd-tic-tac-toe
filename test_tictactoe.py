e931ab8976ddc9c20518e354ec92cead3173c98e
stealthebat <right.seven@yandex.ru>
2023-09-22 19:45:26 +0300

    Second test and move logic

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -11,6 +11,12 @@ class TicTacToeTest(unittest.TestCase):
         self.assertEqual(game.get_current_player(), 'X')
         self.assertIsNone(game.get_winner())
 
+    def test_make_move(self):
+        game = TicTacToe()
+        game.make_move(1)
+        self.assertEqual(game.get_current_player(), 'O')
+        self.assertEqual(game.get_board()[0], 'X')
+
 
 if __name__ == '__main__':
     unittest.main()
--- tictactoe.py
+++ tictactoe.py
@@ -17,6 +17,10 @@ class TicTacToe:
     def get_board(self):
         return self.board
 
+    def make_move(self, position):
+        self.board[position-1] = self.current_player
+        self.current_player = 'O' if self.current_player == 'X' else 'X'
+
     def print_board(self):
         for i in range(3):
             print(*self.board[i*3:3*i+3])
